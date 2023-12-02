4faf0345fce32099a7554b071f672c1bee67e50c
hqqich <hqqich1314@outlook.com>
2023-12-02 16:56:43 +0800

    优化文档

--- tsinglink-tool-test/src/main/java/com/tsinglink/Main.java
+++ tsinglink-tool-test/src/main/java/com/tsinglink/Main.java
@@ -1,24 +1,23 @@
 package com.tsinglink;
 
-import com.tsinglink.csv.EasyCsv;
-import com.tsinglink.csv.entity.Rule;
-import java.util.List;
-
 // 按两次 Shift 打开“随处搜索”对话框并输入 `show whitespaces`，
 // 然后按 Enter 键。现在，您可以在代码中看到空格字符。
 public class Main {
 
 	public static void main(String[] args) {
-		List<TestCsv> csvEntities = new EasyCsv(Rule.builder().split(",").build()).readAll(
-				"D://test2.csv", TestCsv.class
-		);
 
-		csvEntities.forEach(System.out::println);
-
-
-		csvEntities.add(new TestCsv("5", "5", "5", "5"));
-
-		new EasyCsv().write("D://test2.csv", csvEntities);
+        System.out.println(System.getProperty("file.encoding"));
+        //
+		//List<TestCsv> csvEntities = new EasyCsv(Rule.builder().split(",").build()).readAll(
+		//		"D://test2.csv", TestCsv.class
+		//);
+        //
+		//csvEntities.forEach(System.out::println);
+        //
+        //
+		//csvEntities.add(new TestCsv("5", "5", "5", "5"));
+        //
+		//new EasyCsv().write("D://test2.csv", csvEntities);
 
 	}
 }
