ec1d8b18b0693b822f24e5e51a1567c0c6605cbd
hqqich <hqqich1314@outlook.com>
2026-04-12 18:21:20 +0800

    提交代码

--- hqqich-tool-base/src/main/java/io/github/hqqich/tool/base/exception/UtilException.java
+++ hqqich-tool-base/src/main/java/io/github/hqqich/tool/base/exception/UtilException.java
@@ -9,15 +9,23 @@ public class UtilException extends RuntimeException {
 
     private static final long serialVersionUID = 8247610319171014183L;
 
-    public UtilException(Throwable e) {
-        super(e.getMessage(), e);
+    public UtilException() {
+        super();
+    }
+
+    public UtilException(Throwable cause) {
+        super(cause);
     }
 
     public UtilException(String message) {
         super(message);
     }
 
-    public UtilException(String message, Throwable throwable) {
-        super(message, throwable);
+    public UtilException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UtilException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
